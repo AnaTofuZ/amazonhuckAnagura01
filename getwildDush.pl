cf0799fb40c437d1b2b5f4003c4293b0bfe2178e
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-10 19:34:55 +0900

    make HTTP request (delete)

--- getwildDush.pl
+++ getwildDush.pl
@@ -4,4 +4,19 @@ use warnings;
 use utf8;
 
 use Data::Dumper;
+use LWP;
+uaw JSON;
 
+my $ua = LWP::UserAgent->new;
+
+my $req = HTTP::Request->new(GET => 'http://127.0.0.1:3000/getwild');
+
+my $res = $ua->request($req);
+
+    if ($res->is_success) {
+        
+    } else {
+        print $res->status_line,"\n";
+    }
+
+ 
